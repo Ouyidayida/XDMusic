396dd5bb502f87eb263ec96f9cbc44a00e934601
fbb <fanbobo1996@gmail.com>
2025-03-11 18:20:50 +0800

    feat: :sparkles: init

---
+++ assets/js/lang.js
@@ -0,0 +1,4 @@
+const languageData = {
+	"en": { "home": "Home", "brand": "Brand", "news": "News", "about": "About", "dealer": "Dealer", "service": "Service & Support" },
+	"zh": { "home": "首页", "brand": "品牌", "news": "新闻", "about": "关于我们", "dealer": "经销商", "service": "服务与支持" },
+};
